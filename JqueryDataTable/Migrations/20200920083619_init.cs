2023f4627ed236dd313e0ab6094ea47123fc2301
uzair <uzairanwar2299@gmail.com>
2020-09-20 13:36:57 +0500

    Add project files.

---
+++ JqueryDataTable/Migrations/20200920083619_init.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JqueryDataTable.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    iD = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 250, nullable: true),
+                    LastName = table.Column<string>(maxLength: 250, nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(maxLength: 50, nullable: true),
+                    City = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.iD);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
---
+++ JqueryDataTable/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JqueryDataTable.Models
+{
+    public class Customer
+    {
+        public int iD { get; set; }
+        [StringLength(250)]
+        public string FirstName { get; set; }
+
+        [StringLength(250)]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        public string Country { get; set; }
+
+        [StringLength(50)]
+        public string City { get; set; }
+    }
+}
